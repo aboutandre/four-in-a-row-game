001c0b34ef0b9d97968cb3412d61c46c0cead208
aboutandre <github@aboutandre.com>
2018-12-30 11:30:35 +0100

    ADD working board
    All the necessary methods for the board to be drawn are working.

--- My Approach/js/Game.js
+++ My Approach/js/Game.js
@@ -1,3 +1,36 @@
 class Game {
-    
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+
+    /**
+     * Return active player.
+     * @return {Object}
+     */
+    get activePlayer() {
+        return this.players.find(player => player.active);
+    }
+
+    /** 
+    * Creates two player objects
+    * @return  {Array}    An array of two Player objects.
+    */
+    createPlayers() {
+        const players = [
+            new Player('Player 1', 1, '#e15258', true),
+            new Player('Player 2', 2, '#e59a13')
+        ];
+        return players;
+    }
+
+    /** 
+     * Initializes game. 
+     */
+    startGame() {
+        this.board.drawHTMLBoard();
+        this.activePlayer.activeToken.drawHTMLToken();
+        this.ready = true;
+    }
 }
--- My Approach/js/Token.js
+++ My Approach/js/Token.js
@@ -4,4 +4,15 @@ class Token {
         this.id = `token-${index}-${owner.id}`;
         this.played = played;
     }
+    drawHTMLToken() {
+        const htmlToken = document.createElement('div');
+        htmlToken.id = this.id;
+        htmlToken.classList.add('token');
+        htmlToken.style.backgroundColor = this.owner.color;
+        document.getElementById('game-board-underlay').appendChild(htmlToken);
+    }
+    get htmlToken() {
+        const tokenElement = document.getElementById(this.id);
+        return tokenElement;
+    }
 }
